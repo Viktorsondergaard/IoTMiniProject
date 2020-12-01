79810a5965317824b42732acdd9edb3a65558bf9
TobiasDJ <tobiasdammhenrichsen@gmail.com>
2020-12-01 15:08:25 +0100

    Update boid.py

--- boid.py
+++ boid.py
@@ -14,26 +14,23 @@ class boid:
     
     def __init__(self, x, y):
         self.position = vector.Vector(x,y)
-        vec = (np.random.rand(2) - 0.5)*10
+        vec = (np.random.rand(2) - 0.5)*5
         self.velocity = vector.Vector(*vec)
 
 # http://www.vergenet.net/~conrad/boids/pseudocode.html              
-def rule1(boid):
-    
-    pcj = vector.Vector(1,1)
-    N = 1
+def rule1(boid):    
+    pcj = vector.Vector(0,0)
+    N = 0
     for b in flock:
         N += 1
         if b != boid:
-            pcj = pcj - boid.position
-    pcj = pcj / (N-1)
+            pcj = pcj + boid.position
+    pcj = (pcj / (N-1))
     
     return ((pcj-boid.position) / 100)
 
-def rule2(boid):
-    
-    c = vector.Vector(1,1)
-    
+def rule2(boid):    
+    c = vector.Vector(0,0)    
     for b in flock:
         if b != boid:
             if ((abs(b.position - boid.position)) < 100):
@@ -41,23 +38,41 @@ def rule2(boid):
     return c
 
 def rule3(boid):
-    pvj = vector.Vector(1,1)
-    N = 1
+    pvj = vector.Vector(0,0)
+    N = 0
     for b in flock:
         N += 1
         if b != boid:
             pvj = pvj + b.velocity
             
-    pvj = pvj / (N-1)
-    return ((pvj - boid.velocity) / 8)                  
+    pvj = (pvj / (N-1))
+    return ((pvj - boid.velocity) / 8)    
+
+def bound_position(boid):
+    V = vector.Vector(0,0)
+    if boid.position.x < -3000:
+        V.x = 10
+    elif boid.position.x > 3000:
+        V.x = -10
+        
+    if boid.position.y < -3000:
+        V.y = 10        
+    elif boid.position.y > 3000:
+        V.y = -10 
+    return V
+    
+def tend_to_position(boid):
+    place = vector.Vector(2000,2000)
+    return ((place - boid.position) / 100)           
                 
 def move_all_boids_to_new_positions(boids):    
     for b in boids:
         v1 = rule1(b)
         v2 = rule2(b)
         v3 = rule3(b)
-        
-        b.velocity = b.velocity + v1 + v2 + v3
+        v4 = bound_position(b)
+        v5 = tend_to_position(b)        
+        b.velocity = b.velocity + v1 + v2 + v3 + v4 + v5
         b.position = b.position + b.velocity
         
 def draw_boids(boids):
@@ -70,14 +85,15 @@ def draw_boids(boids):
         list_of_boid_y.append(boid.position.y)
 
     plt.scatter(list_of_boid_x, list_of_boid_y)
+    plt.pause(0.1)
     plt.show()
 
 
 
-flock = [boid(*np.random.rand(2)*1000) for _ in range(100)]
+flock = [boid(*np.random.rand(2)*1000) for _ in range(30)]
 
 x = 0
-number = 50
+number = 500
 
 while x < number:
     move_all_boids_to_new_positions(flock)
